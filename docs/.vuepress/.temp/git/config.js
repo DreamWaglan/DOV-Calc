bc814cfee7b9489bfe21a4288302773207efd96c
DreamWaglan <2427999362@qq.com>
2026-05-13 18:10:44 +0800

    Remove tmp file

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "D:/Projects/DOV-Calculator/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_a203cb5b1362c35cb1a86b4535fe5aa2/node_modules/@vuepress/plugin-git/dist/client/components/GitContributors.js";
-import { GitChangelog } from "D:/Projects/DOV-Calculator/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_a203cb5b1362c35cb1a86b4535fe5aa2/node_modules/@vuepress/plugin-git/dist/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
